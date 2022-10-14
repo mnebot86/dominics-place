d5480ad4a4ae18f130d99d9a1acf7cee168a0e08
mnebot <mnebotjr@gmail.com>
2022-10-13 20:37:33 -0400

    Splash Section

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,32 @@
 module.exports = {
-  siteMetadata: {
-    title: `Dominic's Place`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-sass"]
+	siteMetadata: {
+		title: `Dominic's Place`,
+		siteUrl: `https://www.yourdomain.tld`,
+		author: 'Miguel Nebot',
+	},
+	plugins: [
+		'gatsby-plugin-image',
+		'gatsby-plugin-sharp',
+		'gatsby-transformer-sharp',
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'images',
+				path: `${__dirname}/src/images`,
+			},
+		},
+		{
+			resolve: 'gatsby-plugin-layout',
+			options: {
+				component: `${__dirname}/src/components/layout.js`,
+			},
+		},
+		{
+			resolve: 'gatsby-plugin-sass',
+			options: {
+				useResolveUrlLoader: true,
+				sourceMap: true,
+			},
+		},
+	],
 };
---
+++ src/screens/home/splash.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Splash = () => {
+	return (
+		<section className="splash content-wrapper">
+			<div className="background"></div>
+		</section>
+	);
+};
+
+export default Splash;
